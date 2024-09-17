7747e264e3bda4923fcf3bbb9ec3a8d0b2b6f1bd
omar <omarliberatto1967@gmail.com>
2024-09-17 15:47:31 -0300

    nuevos scripts primer_usuario artisan

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-	'url' => env('APP_URL', 'https://plaft.alephmanager.com'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     /*
     |--------------------------------------------------------------------------
