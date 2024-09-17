fbc1c4a12a599c24d453878b172d6b90438a609a
omar <omarliberatto1967@gmail.com>
2024-09-17 15:51:51 -0300

    nuevos scripts primer_usuario artisan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,17 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
+	/**
+	 * Bootstrap any application services.
+	 */
 	public function boot(): void
 	{
 		//
