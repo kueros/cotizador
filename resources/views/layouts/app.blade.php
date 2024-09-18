e97814acde63555559792846180b6365b9f63ce0
omar <omarliberatto1967@gmail.com>
2024-09-18 17:01:41 -0300

    no mas vite!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,9 @@
 	<link rel="preconnect" href="https://fonts.bunny.net">
 	<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 	<link href="https://cdn.datatables.net/2.1.5/css/dataTables.dataTables.css" rel="stylesheet">
-	<link href="/build/assets/awesome/css/solid.css" rel="stylesheet">
+	<link href="/build/assets/css/ex-vite1.css" rel="stylesheet">
 	<link href="https://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" rel="Stylesheet">
 	<!-- Scripts -->
-	@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,9 +22,9 @@
 	<!-- Fonts -->
 	<link rel="preconnect" href="https://fonts.bunny.net">
 	<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+	<link href="/build/assets/css/ex-vite1.css" rel="stylesheet">
 
 	<!-- Scripts -->
-	@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
