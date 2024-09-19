e72cf5c8af0f87e7f1c5fc53717e2b36c3f2a0bd
omar <omarliberatto1967@gmail.com>
2024-09-18 23:39:36 -0300

    -

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
 			<!-- Session Status -->
 			<!-- x-auth-session-status class="mb-4" :status="session('status')" / -->
 
-			<form method="POST" action="{{ secure_route('login') }}">
+			<form method="POST" action="{{ route('login') }}">
 				@csrf
 				<div>
 					<a href="/">
