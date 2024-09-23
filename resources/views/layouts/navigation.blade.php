ad8347a62c2677c9fdbf10b71bd032fc49bf4143
omarliberatto <omarliberatto@yafoconsultora.com>
2024-09-23 12:52:10 -0300

    rutas logout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -79,10 +79,10 @@
 						</x-dropdown-link>
 
 						<!-- Authentication -->
-						<form method="POST" action="{{ route('logout') }}">
+						<form method="POST" action="url('logout')">
 							@csrf
 
-							<x-dropdown-link :href="{{ route('logout') }}"
+							<x-dropdown-link :href="url('logout')"
 								onclick="event.preventDefault();
                                                 this.closest('form').submit();">
 								{{ __('Log Out') }}
@@ -125,11 +125,11 @@
 				</x-responsive-nav-link>
 
 				<!-- Authentication -->
-				<form method="POST" action="{{ route('logout') }}">
+				<form method="POST" action="url('logout')">
 					@csrf
 					<input type="hidden" name="email" value="{{ $email }}">
 					<input type="hidden" name="username" value="{{ $user }}">
-					<x-responsive-nav-link :href="route('logout')"
+					<x-responsive-nav-link :href="url('logout')"
 						onclick="event.preventDefault();
                                         this.closest('form').submit();">
 						{{ __('Log Out') }}
