ba1828e15e32fe9c87eb367115e86e1c57f4bf2f
root <root@yafoconsutora.com>
2024-10-22 19:45:09 +0000

    updates

---
+++ app/Models/Permiso_x_Rol.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permiso_x_rol extends Model
+{
+
+	use HasFactory;
+
+	protected $table = 'permisos_x_rol'; // Asegúrate de que el nombre de la tabla es correcto
+
+	protected $fillable = [
+		'rol_id',
+		'nombre',
+		'descripcion',
+		'permiso_id',
+		'habilitado',
+	];
+
+}
